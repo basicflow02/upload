fe58bca51bb5a081ed07dc6a2a5c34aa4e623e59
basicflow02 <145775446+basicflow02@users.noreply.github.com>
2023-09-22 19:08:02 +0530

    Add files via upload

---
+++ upload/upload/upload/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>college stuff</title>
+</head>
+<body>
+
+<?php
+if(isset($_POST['submit'])){
+	$filename = $_FILES['file']['name'];
+	$tmpname = $_FILES['file']['tmp_name'];
+	$filesize = $_FILES['file']['size'];
+	$filetype = $_FILES['file']['type'];
+	$folder = 'upload/';
+
+	move_uploaded_file($tmpname, $folder.$filename);
+
+	echo "File uploaded successfully";
+}
+?>
+
+<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="file">
+	<br><br>
+	<input type="submit" name="submit" value="Upload">
+</form>
+
+<a href="upload/">link</a>
+</body>
+</html>
